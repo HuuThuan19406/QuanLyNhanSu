bb8b211bf79f2b88eb76014611e153c56591c31c
HuuThuan19406 <thuannh19406@st.uel.edu.vn>
2020-04-27 10:44:56 +0700

    Fix unsync

--- QuanLyNhanSu/VIEW/UserControls/ListPhongBan.xaml.cs
+++ QuanLyNhanSu/VIEW/UserControls/ListPhongBan.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuanLyNhanSu.MODULE.XyLuDatabase;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections;
 
 namespace QuanLyNhanSu
 {
@@ -23,8 +25,21 @@ namespace QuanLyNhanSu
         public ListBoPhan()
         {
             InitializeComponent();
-            this.DataContext = this;
-            comBoBox.ItemsSource = MainWindow.danhSachPhongBan;
+            this.DataContext = this;            
+            listView.MouseDoubleClick += (s, a) => listView.SelectedIndex = -1;
+        }
+
+        private void comBoBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(comBoBox.SelectedIndex==-1)
+            {
+                listView.ItemsSource = null;
+                listView.Items.Clear();
+                return;
+            }            
+            listView.ItemsSource = null;
+            listView.Items.Clear();
+            listView.ItemsSource = MainDatabase.dsNhanSu.Values.Cast<NhanSu>().Where(p => p.BoPhan == comBoBox.SelectedItem.ToString());
         }
     }
 }
